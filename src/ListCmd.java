9d1293bbde1b5b4a696e1059cc19ce85f4d7c4ad
Advaith Sai <advaith.maddipatla@gmail.com>
2020-03-16 18:20:45 +0000

    done with 4

--- src/ListCmd.java
+++ src/ListCmd.java
@@ -1,9 +1,55 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
 public class ListCmd extends LibraryCommand {
+    String shortArgument;
+
+    /**String for long listing*/
+    public final String LONG = "long";
+    /**Strings for short listing*/
+    public final String SHORT_1 = "short";
+    public final String SHORT_2 = "";
     public ListCmd(String shortArgument) {
+        super(CommandType.LIST,shortArgument);
+        this.shortArgument = shortArgument;
     }
 
     @Override
     public void execute(LibraryData data) {
+        // TODO should i check the parseArguments part
+        Objects.requireNonNull(data,"Cannot be null");
+        List<BookEntry> listCmdBooks = data.getBookData();
+        Iterator<BookEntry> iterator = listCmdBooks.iterator();
+        StringBuilder listOutput = new StringBuilder();
+        if (listCmdBooks.size() == 0) {
+            listOutput.append("The library has no book entries.");
+        }
+        else {
+            listOutput.append(listCmdBooks.size()+" books in library:\n");
+            if (shortArgument.equalsIgnoreCase(SHORT_1) || shortArgument.equalsIgnoreCase(SHORT_2)) {
+                while (iterator.hasNext()) {
+                    BookEntry book = iterator.next();
+                    listOutput.append(book.getTitle()).append('\n');
+                }
+            }
+            else if (shortArgument.equalsIgnoreCase(LONG)) {
+                while (iterator.hasNext()){
+                    BookEntry book = iterator.next();
+                    listOutput.append(book.toString());
+                    listOutput.append("\n\n");
+                }
+            }
+        }
 
+        System.out.println(listOutput);
+    }
+    @Override
+    protected boolean parseArguments(String listArgument) {
+        Objects.requireNonNull(listArgument,"Cannot be null");
+        shortArgument = listArgument;
+        return  listArgument.equalsIgnoreCase(LONG) ||
+                listArgument.equalsIgnoreCase(SHORT_1) ||
+                listArgument.equalsIgnoreCase(SHORT_2);
     }
 }
--- src/SearchCmd.java
+++ src/SearchCmd.java
@@ -1,9 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SearchCmd extends LibraryCommand {
-    public SearchCmd(String singleWordTitle) {
+    String searchWord;
+    public SearchCmd(String searchWord) {
+        super(CommandType.SEARCH,searchWord);
+        this.searchWord = searchWord;
     }
 
     @Override
     public void execute(LibraryData data) {
-
+        List<BookEntry> booksList = data.getBookData();
+        List<String> titleList = new ArrayList<>();
+        for (BookEntry book : booksList) {
+            titleList.add(book.getTitle());
+        }
+        System.out.println(Arrays.toString(titleList.toArray()));
+    }
+    public String[] inString(String[] matchCases) {
+        return null;
+    }
+    @Override
+    protected  boolean parseArguments(String singleWordSearch) {
+        Objects.requireNonNull(singleWordSearch,"Cannot be a null Object");
+        searchWord = singleWordSearch;
+        if (singleWordSearch.equals("")) {
+            return false;
+        }
+        return singleWordSearch.split(" ",0).length == 1;
     }
 }
