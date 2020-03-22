eaa0a738e4d34c0d75fd4df81c2f58f99efb21cc
Advaith Sai <advaith.maddipatla@gmail.com>
2020-03-22 11:33:01 +0000

    group cmd ongoing

--- src/GroupCmd.java
+++ src/GroupCmd.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class GroupCmd extends LibraryCommand {
@@ -9,11 +12,20 @@ public class GroupCmd extends LibraryCommand {
 
     @Override
     public void execute(LibraryData data) {
-
+        List<BookEntry> books = data.getBookData();
+        Map<String, List<String>> groupMap = new HashMap<String, List<String>>();
+        if(groupArgument.equals(RemoveCmd.TITLE)) {
+            for (BookEntry book : books) {
+                if (Character.isAlphabetic(book.getTitle().charAt(0))) {
+                    
+                }
+            }
+        }
     }
     @Override
     protected boolean parseArguments(String groupInput) {
         groupArgument = groupInput;
+        Objects.requireNonNull(groupInput);
         return groupInput.equals(RemoveCmd.AUTHOR) || groupInput.equals(RemoveCmd.TITLE);
     }
 }
