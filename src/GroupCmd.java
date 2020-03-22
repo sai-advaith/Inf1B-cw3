30a3fdb655e2996b0a44e75c13960a70d0b1d956
Advaith Sai <advaith.maddipatla@gmail.com>
2020-03-22 16:31:27 +0400

    title printing left

--- src/GroupCmd.java
+++ src/GroupCmd.java
@@ -1,7 +1,5 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.awt.print.Book;
+import java.util.*;
 
 public class GroupCmd extends LibraryCommand {
     String groupArgument;
@@ -15,12 +13,37 @@ public class GroupCmd extends LibraryCommand {
         List<BookEntry> books = data.getBookData();
         Map<String, List<String>> groupMap = new HashMap<String, List<String>>();
         if(groupArgument.equals(RemoveCmd.TITLE)) {
-            for (BookEntry book : books) {
-                if (Character.isAlphabetic(book.getTitle().charAt(0))) {
-                    
+            groupMap = titleGroup(books);
+        }
+    }
+    public String mapOutput(Map<String,List<String>> groupArg) {
+           StringBuilder output = new StringBuilder();
+           if (groupArg.size() == 0) {
+               output.append("The library has no book entries.");
+           }
+           else {
+               output.append()
+           }
+    }
+    public Map<String,List<String>> titleGroup(List<BookEntry> books) {
+        String nonNumericTitle = "[0-9]";
+        Map<String, List<String>> titleMap = new HashMap<String, List<String>>();
+        for (BookEntry book : books) {
+            if (Character.isAlphabetic(book.getTitle().charAt(0))) { // checking the first letter
+                String titleCategory = String.valueOf(book.getTitle().charAt(0));
+                if (!titleMap.containsKey(titleCategory)) {
+                    titleMap.put(titleCategory, new ArrayList<>());
                 }
+                titleMap.get(titleCategory).add(book.getTitle());
+            }
+            else {
+                if (!titleMap.containsKey(nonNumericTitle)) {
+                    titleMap.put(nonNumericTitle,new ArrayList<>());
+                }
+                titleMap.get(nonNumericTitle).add(book.getTitle());
             }
         }
+        return titleMap;
     }
     @Override
     protected boolean parseArguments(String groupInput) {
